af1402d9be87a0df1a2c816513d6e121596236e1
anhTuan2797 <65809423+anhTuan2797@users.noreply.github.com>
2020-07-16 20:47:58 +0700

    add key event

--- source/main.js
+++ source/main.js
@@ -38,8 +38,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const allBlock = [oBlock, iBlock, lBlock, zBlock, tBlock];
 
     let currentPosition = 6;
-    let randomBlockIndex;
-    let randomBlockPosition;
+    let blockIndex;
+    let blockRotation;
     let currentBlock;
     let status = 'stopped';
     let intervalId = 0;
@@ -58,12 +58,12 @@ document.addEventListener('DOMContentLoaded', () => {
                     moveDown();
                 }
             }, 1000);
-        }else if(status == 'running'){
+        } else if (status == 'running') {
             status = 'paused';
             button.style.backgroundColor = '#99ff66';
             button.innerText = 'start';
             clearInterval(intervalId);
-        } else if(status == 'paused'){
+        } else if (status == 'paused') {
             status = 'running';
             button.style.backgroundColor = '#ff3300';
             button.innerText = 'pause';
@@ -76,6 +76,33 @@ document.addEventListener('DOMContentLoaded', () => {
             }, 1000);
         }
     })
+    document.addEventListener('keydown', function (e) {
+        if (e.keyCode == 38) {
+            if (status == 'running') {
+                rotate();
+            }
+        }
+        if (e.keyCode == 40) {
+            if (status == 'running'){
+                if (checkFloor() || checkLandedBlock()) {
+                    freeze();
+                } else {
+                    moveDown();
+                }
+            }
+        }
+        if (e.keyCode == 37) {
+            if (status == 'running'){
+                moveLeft();
+        }
+    }
+        if(e.keyCode == 39) {
+            if (status == 'running'){
+                console.log('move right');
+                moveRight();
+        }
+    }
+    });
     // draw a block function
     function draw() {
         currentBlock.forEach(index => {
@@ -89,7 +116,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    // check for landed block function
+    // check for landed block function use for fall down block
     function checkLandedBlock() {
         if (currentBlock.some(index => squares[currentPosition + index + width].classList.contains('landed-block'))) {
             return true;
@@ -97,7 +124,14 @@ document.addEventListener('DOMContentLoaded', () => {
             return false;
         }
     }
-
+    // check for landed block function use for moving block
+    function checkLandedBlockForMove(nextPosition){
+        if (currentBlock.some(index => squares[currentPosition + index + nextPosition].classList.contains('landed-block'))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
     // check for floor function
     function checkFloor() {
         if (currentBlock.some(index => squares[currentPosition + index].classList.contains('floor'))) {
@@ -105,6 +139,15 @@ document.addEventListener('DOMContentLoaded', () => {
         } else {
             return false;
         }
+    }
+    // check if the block is at far right function 
+    function isAtRight(){
+        return currentBlock.some(index => (currentPosition + index +1)% width === 0)
+    }
+
+    // check if the block is at far left function 
+    function isAtLeft(){
+        return currentBlock.some(index => (currentPosition + index )% width === 0)
     }
 
     // move a block down function
@@ -116,9 +159,9 @@ document.addEventListener('DOMContentLoaded', () => {
     // make a random block function
     function makeARandomBlock() {
         currentPosition = 6;
-        randomBlockIndex = Math.floor(Math.random() * 5);
-        randomBlockPosition = Math.floor(Math.random() * 4);
-        currentBlock = allBlock[randomBlockIndex][randomBlockPosition];
+        blockIndex = Math.floor(Math.random() * 5);
+        blockRotation = Math.floor(Math.random() * 4);
+        currentBlock = allBlock[blockIndex][blockRotation];
     }
     // freeze a block function 
     function freeze() {
@@ -127,5 +170,48 @@ document.addEventListener('DOMContentLoaded', () => {
             squares[currentPosition + index].classList.add('landed-block');
         });
         makeARandomBlock();
+    }
+    // rotate block function
+    function rotate(){
+        unDraw();
+        blockRotation++;
+        if(blockRotation === currentBlock.length){
+            blockRotation =0;
+        }
+        currentBlock = allBlock[blockIndex][blockRotation];
+        checkRotatedPosition();
+        draw();
+    }
+    // check rotated position
+    function checkRotatedPosition(p){
+        p = p || currentPosition;
+        if((p+1) % width < 5){
+            if(isAtRight()){
+                currentPosition++;
+                checkRotatedPosition(p);
+            }
+        }
+        else if(p % width > 6){
+            if(isAtLeft()){
+                currentPosition--;
+                checkRotatedPosition(p);
+            }
+        }
+    }
+    // move left function 
+    function moveLeft() {
+        if(!isAtLeft() && !checkLandedBlockForMove(-1)){
+            unDraw();
+            currentPosition--;
+            draw();
+        }
+    }
+    // move right function 
+    function moveRight(){
+            if(!isAtRight() && !checkLandedBlockForMove(1)){
+                unDraw();
+                currentPosition++;
+                draw();
+            }
     }
 });
